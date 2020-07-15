a4fa8e040078cde124438e3278a832ad48341a78
vinceau <vinceau@users.noreply.github.com>
2020-07-15 01:59:10 +0000

    Automated deployment: Wed Jul 15 01:59:10 UTC 2020 35e1d3eb176db82e9316f93593898b09474821f8

---
+++ precache-manifest.00a53461e50e82778d9e2b1757d134e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54886bc1de5feaf4238290d1aeb1c399",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21c2860ab04333eede19",
+    "url": "./static/css/main.c92c3d9f.chunk.css"
+  },
+  {
+    "revision": "9474dd0c97f0a14690d4",
+    "url": "./static/js/2.e3e6475b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "./static/js/2.e3e6475b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c2860ab04333eede19",
+    "url": "./static/js/main.7f8de310.chunk.js"
+  },
+  {
+    "revision": "4a57efdd58f3e50dcfe3",
+    "url": "./static/js/runtime-main.89c843c7.js"
+  }
+]);
